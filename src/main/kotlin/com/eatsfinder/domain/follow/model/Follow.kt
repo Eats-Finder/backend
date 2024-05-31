a34829d0f688254ad6e30f816cd97db4d40188af
rim109 <fla7722@naver.com>
2024-05-31 15:07:48 +0900

    ⭐Feat: follow 엔티티 작성
    -#1

---
+++ src/main/kotlin/com/eatsfinder/domain/follow/model/Follow.kt
@@ -0,0 +1,32 @@
+package com.eatsfinder.domain.follow.model
+
+import com.eatsfinder.domain.user.model.User
+import jakarta.persistence.*
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import org.springframework.data.annotation.CreatedDate
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "follows")
+class Follow(
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_user_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    val followedUserId: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_user_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    val followingUserId: User,
+
+    ) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+}
